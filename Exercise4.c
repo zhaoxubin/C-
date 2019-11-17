2e647380f31adec13d902ce70b175fa94929d0a2
zhaoxubin <55921289+zhaoxubin@users.noreply.github.com>
2019-11-17 14:14:53 +0800

    利用操作符实现不用中间变量交换两个整形的内容更加方便
    这里主要使用了异或操作符：按比特位看每个对应比特位相同为0，不同为1；所以两个相同的整数对应比特位相同，所以相异或就为0，
    0与任何数异或都为这个数。

--- Exercise4.c
+++ Exercise4.c
@@ -1,11 +1,12 @@
-
 #include<stdio.h>
 int main()
 {
-	int a=4, b=5;
-	a = b + a;
-	b = a - b;    /* b=b+a-b-->b=a */
-	a = a - b;     /*a=a-a-b-->a=b*/
-	printf("a=%d\n", a);
-	printf("b=%d\n", b);
+	int a = 4, b = 5;
+	a = a^b;
+	b = a^b;    /*因为a=a^b所以 b=a^b^b 其中b^b=0,则a^0=a,最终b=a */
+	a = a^b;   /*因为b=a^b所以 b=a^a^b 其中a^a=0,则b^0=b,最终a=b */
+	printf("a=%d   b=%d", a, b);
 }
+
+
+
