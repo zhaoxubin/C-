fb941c716f747c80e439657f21694eca891bc84f
zhaoxubin <55921289+zhaoxubin@users.noreply.github.com>
2019-11-17 17:18:04 +0800

    输入连个数，输出这两个数的最大公约数
    改进后使用了一个for循环即可实现，把判定法放到前面，用第三变量接收ab较小数，并控制循环，完成寻找

--- Exercise7.c
+++ Exercise7.c
@@ -3,33 +3,24 @@
 #pragma warning(disable:4996)
 int main()
 {
-	int a, b, i = 0;
+	int a, b, i = 0,temp=0;
 	printf("please entertwo intger num");
 	scanf("%d %d", &a, &b);
 	if (a > b){
-		for (i = b; i > 0; i--)
-		{
-			if (a%i == 0 && b%i == 0)
-			{
-				printf("%d/n", i);
-				break;
-			}
-
-		}
+		temp = b;
 	}
 	else
 	{
-		for (i = a; i > 0; i--)
+		temp = a;
+	}
+
+		for (i = temp; i > 0; i--)
 		{
 			if (a%i == 0 && b%i == 0)
 			{
 				printf("%d/n", i);
 				break;
 			}
-
 		}
-
-	}
 		system("pause");
 		return(0);
-}
